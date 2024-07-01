25e122dcfc58b70ed6b4656a8db8f9e14d6b42d2
Keila <keilaprado@alunos.utfpr.edu.br>
2024-07-01 16:33:56 -0300

    fiz mesmas alterações da página de erro de transição suave na página de sucesso

--- desafio1/js/script-success.js
+++ desafio1/js/script-success.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const video = document.getElementById('bg-video')
     const content = document.getElementById('content')
+    const menu = document.querySelector('.container1')
+    const footer = document.querySelector('.container3')
     const urlParams = new URLSearchParams(window.location.search)
     const userName = urlParams.get('username')
 
@@ -11,8 +13,12 @@ document.addEventListener('DOMContentLoaded', function(){
 
     video.addEventListener('canplaythrough', function(){
         content.classList.remove('hidden')
+        menu.classList.remove('hidden')
+        footer.classList.remove('hidden')
         setTimeout(function(){
             content.classList.add('show')
+            menu.classList.add('show')
+            footer.classList.add('show')
         }, 60)
     })
 })
