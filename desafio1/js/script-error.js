bbea6d784158e613ddae05665ccaf5ca1abd2011
Keila <keilaprado@alunos.utfpr.edu.br>
2024-07-01 16:29:40 -0300

    adicionei transição suave ao header e footer

--- desafio1/js/script-error.js
+++ desafio1/js/script-error.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     const video = document.getElementById('bg-video')
     const content = document.getElementById('content')
+    const menu = document.querySelector('.container1')
+    const footer = document.querySelector('.container3')
 
     video.addEventListener('canplaythrough', function(){
         content.classList.remove('hidden')
+        menu.classList.remove('hidden')
+        footer.classList.remove('hidden')
         setTimeout(function(){
             content.classList.add('show')
+            menu.classList.add('show')
+            footer.classList.add('show')
         }, 60)
     })
 })
